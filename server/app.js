ea5d5db1240cd42f18e4856a82f5a6bb79fd030f
flo20 <mawuaniipa@gmail.com>
2021-09-13 22:45:38 +0300

    #

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const {graphqlHTTP} = require("express-graphql");
 /*the installed express-graphql allows express to understand graphql. 
 It allows a simple way to create a server that runs the graphql api */
 const schema = require("./schema/schema");
+const cors = require("cors");
 require('dotenv').config();
 const mongoose= require("mongoose");
 
 
 const app = express();
+//allow cross origin
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
